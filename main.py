54fd470bbc339e64ab5957998ba50b75a8c9b502
Delvin1of1 <159158944+Delvin1of1@users.noreply.github.com>
2025-01-22 17:42:49 -0500

    update main.py: add age functionality

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 print("Hello, world!")
 name = input("what is your name?")
 print(f"Nice to meet you, {name}!")
+age = int(input("how old are you"))
+print(f"you will be {age + 1} years old next year!")
 
       
